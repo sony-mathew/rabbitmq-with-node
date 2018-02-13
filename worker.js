4aee74e027b5acc2d2765fe572e0df66c15a56fe
Sony Mathew <ynos1234@gmail.com>
2018-02-13 23:33:13 +0530

    3 - Receiving messages from queue

---
+++ worker.js
@@ -0,0 +1,11 @@
+var amqp = require('amqplib/callback_api');
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+    ch.assertQueue(q, { durable: true });
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    }, { noAck: true });
+  });
+});
